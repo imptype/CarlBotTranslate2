f882577f6fd7632271171c5656c400819460bced
imp <106537315+imptype@users.noreply.github.com>
2023-03-19 17:12:27 +0000

    Update Stats.py

--- src/classes/Stats.py
+++ src/classes/Stats.py
@@ -10,11 +10,9 @@ class Stats:
     self.requests = 0 # total requests (not just image route)
     self.waiting = 0 # waiting in queue
     self.timeout = 0 # timed out for waiting too long
-    self.skipped = 0 # skipped waiting when couldn't find element
     self.malformed = 0 # request malformed count
     self.success = 0 # success image request
-    self.failed = 0 # image failed request, and hopefully logs to discord.log unless repl.it files fail
-    self.toobig = 0 # failed because image too big
-    self.pinged = 0 # ping count
+    self.failed = 0 # image failed request, should record error in deta space logs
     self.usecache = 0 # used from cache count
+    self.transfer = 0 # total bytes transferred
     # self.unknown for non-discord/google
