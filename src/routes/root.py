5f417a525a5e9848d9c555a346404554d4a39a1b
imp <106537315+imptype@users.noreply.github.com>
2023-03-19 17:24:38 +0000

    Update root.py

--- src/routes/root.py
+++ src/routes/root.py
@@ -12,7 +12,7 @@ router = APIRouter()
 async def root(request : Request):
   stats = request.app.stats
   cache = request.app.cache
-  lock = request.app.lock
+  semaphore = request.app.semaphore
   return HTMLResponse('<br>'.join([
     'API is online.',
     '',
@@ -20,17 +20,15 @@ async def root(request : Request):
     'Requests: {:,}'.format(stats.requests),
     'Waiting: {:,}'.format(stats.waiting),
     'Timeout: {:,}'.format(stats.timeout),
-    'Skipped: {:,}'.format(stats.skipped),
     'Malformed: {:,}'.format(stats.malformed),
     'Success: {:,}'.format(stats.success),
     'Failed: {:,}'.format(stats.failed),
-    'Toobig: {:,}'.format(stats.toobig),
-    'Pinged: {:,}'.format(stats.pinged),
     'Usecache: {:,}'.format(stats.usecache),
+    'Transfer: {:,}'.format(stats.transfer),
     'Cached: {:,}'.format(len(cache)),
     'Size: {:,}'.format(cache._last_bytes),
-    'Busy: {}'.format(lock.locked()),
+    'Available: {}'.format(semaphore._value),
     '',
-    'Source: <a href="https://replit.com/@imp7/CarlBotTranslate">https://replit.com/@imp7/CarlBotTranslate</a>',
-    'Github: <a href="https://github.com/imptype/CarlBotTranslate">https://github.com/imptype/CarlBotTranslate</a>'
+    'Source: <a href="https://replit.com/@imp7/CarlBotTranslateV2">https://replit.com/@imp7/CarlBotTranslateV2</a>',
+    'Github: <a href="https://github.com/imptype/CarlBotTranslateV2">https://github.com/imptype/CarlBotTranslateV2</a>'
   ]))
